b03a65c1e8e5b8a6fa80dd97844443e4910ce02d
panneer2004 <153737187+panneer2004@users.noreply.github.com>
2024-01-03 13:28:03 +0000

    done

---
+++ findodd(or)evenbymulitipy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int num = 25;
+    
+    if ( num%2 == 0 ){
+    
+        cout << "Even..";
+}
+        else {
+            cout << "Odd..";
+        }
+        
+        
+
+return  0;
+}
